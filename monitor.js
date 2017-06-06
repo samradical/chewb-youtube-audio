8546fcff240b370bad65bfff927744243e56a9e2
samelie <sam@stinkdigital.com>
2017-06-06 14:37:17 -0700

    bug fix.

--- monitor.js
+++ monitor.js
@@ -1,21 +1,19 @@
-import Signals from 'signals'
-const M = (() => {
+let Signals = require('signals')
+module.exports = (() => {
 
-  let _started = true
-  let _peaks, _drops, _sound
+	let _started = true
+	let _peaks, _drops, _sound
 
-  const onPeakSignal = new Signals()
-  const onDropSignal = new Signals()
+	const onPeakSignal = new Signals()
+	const onDropSignal = new Signals()
 
-  const _setInactive = (obj) => {
-    obj.active = false
-  }
+	const _setInactive = (obj) => {
+		obj.active = false
+	}
 
-  const _setActive = (obj) => {
-    obj.active = true
-  }
-
-
+	const _setActive = (obj) => {
+		obj.active = true
+	}
 
 
 
@@ -23,77 +21,77 @@ const M = (() => {
 
 
 
-  function start() {
-    _started = true
-  }
-
-  function stop() {
-    _started = false
-  }
-
-  function update() {
-    if (!_started || !_sound) {
-      return
-    }
-    let time = _sound.progress * _sound.duration
-    if (_peaks) {
-      /*
-      { start: _start, end: _start + _dur }
-      */
-      for (var i = 0; i < _peaks.length; i++) {
-        let obj = _peaks[i]
-        if (time > obj.start && time < obj.end) {
-          if (!obj.active) {
-            _setActive(obj)
-            onPeakSignal.dispatch(obj)
-          }
-        } else {
-          _setInactive(obj)
-        }
-      }
-    }
-    if (_drops) {
-      /*
-      { start: _start, end: _start + _dur }
-      */
-      for (var i = 0; i < _drops.length; i++) {
-        let obj = _drops[i]
-        if (time > obj.start && time < obj.end) {
-          if (!obj.active) {
-            _setActive(obj)
-            onDropSignal.dispatch(obj)
-          }
-        } else {
-          _setInactive(obj)
-        }
-      }
-    }
-  }
-
-  function setPeaks(peaks) {
-    _peaks = [...peaks]
-  }
-
-  function setDrops(drops) {
-    _drops = [...drops]
-  }
-
-  function setSound(sound){
-  	_sound = sound
-  }
 
 
-  return {
-    start: start,
-    stop: stop,
-    update: update,
-    setSound: setSound,
-    setPeaks: setPeaks,
-    setDrops: setDrops,
-    onPeakSignal: onPeakSignal,
-    onDropSignal: onDropSignal,
-  }
+	function start() {
+		_started = true
+	}
+
+	function stop() {
+		_started = false
+	}
+
+	function update() {
+		if (!_started || !_sound) {
+			return
+		}
+		let time = _sound.progress * _sound.duration
+		if (_peaks) {
+			/*
+			{ start: _start, end: _start + _dur }
+			*/
+			for (var i = 0; i < _peaks.length; i++) {
+				let obj = _peaks[i]
+				if (time > obj.start && time < obj.end) {
+					if (!obj.active) {
+						_setActive(obj)
+						onPeakSignal.dispatch(obj)
+					}
+				} else {
+					_setInactive(obj)
+				}
+			}
+		}
+		if (_drops) {
+			/*
+			{ start: _start, end: _start + _dur }
+			*/
+			for (var i = 0; i < _drops.length; i++) {
+				let obj = _drops[i]
+				if (time > obj.start && time < obj.end) {
+					if (!obj.active) {
+						_setActive(obj)
+						onDropSignal.dispatch(obj)
+					}
+				} else {
+					_setInactive(obj)
+				}
+			}
+		}
+	}
+
+	function setPeaks(peaks) {
+		_peaks = [...peaks]
+	}
+
+	function setDrops(drops) {
+		_drops = [...drops]
+	}
+
+	function setSound(sound){
+		_sound = sound
+	}
+
+
+	return {
+		start: start,
+		stop: stop,
+		update: update,
+		setSound: setSound,
+		setPeaks: setPeaks,
+		setDrops: setDrops,
+		onPeakSignal: onPeakSignal,
+		onDropSignal: onDropSignal,
+	}
 
 })()
-
-module.exports = M
